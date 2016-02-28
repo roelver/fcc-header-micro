a18ff3f8a80400194240c02c4c9a8d750adf89a5
roelver <roel@romaniflo.nl>
2016-02-28 16:27:45 +0100

    Minor fixes to get it running on Heroku

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
 routes(app);
 
-app.listen(3000, function () {
-   console.log('Node.js listening on port 3000...');
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+   console.log('Node.js listening on port '+port+'...');
 });
