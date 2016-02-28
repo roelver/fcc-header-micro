453c1ff63cf61bf0e106462806a65fd13075d02c
roelver <roel@romaniflo.nl>
2016-02-28 15:55:39 +0100

    Initial commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,31 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+module.exports = function (app) {
 
-module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+   app.route('/info')
+		.get(function (req, res) {
+			res.status(200).send(showHeaderInfo(req));
+		});
 
    app.route('/')
       .get(function (req, res) {
          res.sendFile(process.cwd() + '/public/index.html');
       });
 
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+	var showHeaderInfo = function(req) {
+		var ip = req.ip;
+		var pos1 = ip.lastIndexOf(":");
+		ip = ip.substring(pos1);
+
+		var lang = req.header('Accept-Language');
+		pos1 = lang.indexOf(',');
+		lang = lang.substring(0,pos1);
+
+		var agent = req.header('User-Agent');
+		pos1 = agent.indexOf('(')+1;
+		var pos2 = agent.indexOf(')', pos1);
+		agent = agent.substring(pos1,pos2);
+		return { ipaddress: ip, language: lang, software: agent};
+	}
+
 };
